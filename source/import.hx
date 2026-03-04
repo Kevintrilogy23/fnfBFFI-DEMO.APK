1b9ae10cc90800453f592838ca6b548ef3b0d5c2
Kevintrilogy23 <sonicgamesfart@gmail.com>
2026-03-04 18:11:40 -0300

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -2,9 +2,15 @@
 // Discord API
 import funkin.backend.DiscordClient;
 
+// Adicionado para suportar Android (Baseado no JoseXD2)
+#if android
+import android.AndroidControls;
+import android.flixel.FlxVirtualPad;
+import android.flixel.FlxHitbox;
+#end
+
 #if sys
 import sys.*;
-
 import sys.io.*;
 #end
 
